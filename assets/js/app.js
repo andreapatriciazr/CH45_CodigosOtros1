9433e98041115b16972ab99cd4f40f5ca84a307e
Andrea Patricia <andreapatriciazr@gmail.com>
2024-09-26 11:00:46 -0700

    Cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
+
+const noBlog = 'No blog disponible';
+const noLocation = 'No location available';
 
 function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+
+  const data = await response.json();
   console.log(data);
   $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $b.textContent = '${data.blog}' || noBlog;
+  $l.textContent = '${data.location}' || noLocation;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
